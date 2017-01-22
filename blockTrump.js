e5f67896baa9860ca40c052514ab317fcc3a815f
bmwags182 <bretwagner@bwagner-webdev.com>
2017-01-22 09:07:02 -0600

    v0.1.1
    trying to fix and rerun script on scroll

--- blockTrump.js
+++ blockTrump.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trump Killer
 // @namespace    http://localhost
-// @version      0.1
+// @version      0.1.1
 // @description  try to block trump!
 // @author       Bret Wagner
 // @match        https://*.facebook.com/*
@@ -10,6 +10,12 @@
 
 (function() {
     'use strict';
+    var baseState = history.state;
+    blockTrump();
+})();
+
+function blockTrump() {
+	var baseState = history.state;
     var posts = document.getElementsByClassName("userContentWrapper _5pcr");
     for(var i=0; i<posts.length; i++) {
         if (posts[i].innerHTML.toLowerCase().includes("trump")) {
@@ -17,4 +23,24 @@
             console.log("hidden");
         }
     }
-    })();
+    sleep(30000);
+    stateChanged(baseState, getCurrentState());
+}
+
+function getCurrentState() {
+    var stateNow = history.state;
+    return stateNow;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function stateChanged(baseState, stateNow) {
+    if (baseState != stateNow) {
+        baseState = stateNow;
+        blockTrump();
+    } else {
+        sleep(30000);
+    }
+}
