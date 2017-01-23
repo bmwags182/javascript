1233f9eedcd39c161ccb3191affefe68873941eb
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-23 16:41:47 -0600

    save before heading home
    Still working on a little bit here, right now I need to make a function
    to request the images from flickr, as well as get an API key from them
    and setup that in another file for storage without uploading my api key
    into git.

--- Trump Killer.user.js
+++ Trump Killer.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Trump Killer
-// @namespace    http://localhost
-// @version      0.3.3
-// @description  try to block the trump!
-// @author       Bret Wagner
-// @match        https://*.facebook.com/*
-// @grant        GM_xmlhttpRequest
+// @name            Trump Killer
+// @namespace       http://localhost
+// @version         0.3.3
+// @description     try to block the trump!
+// @author          Bret Wagner
+// @match           https://*.facebook.com/*
+// @require         http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @grant           GM_xmlhttpRequest
 // @grant
 // ==/UserScript==
 
@@ -15,6 +16,21 @@
     document.addEventListener("scroll", scrollFunction, {passive:true});
     blockTrump();
 })();
+
+// Fix for jQuery so as not to break the page
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+
+var kitten = {
+        "id"        : "",
+        "owner"     : "",
+        "secret"    : "",
+        "server"    : "",
+        "title"     : "",
+        "isPublic"  : "",
+        "isFriend"  : "",
+        "isFamily"  : ""
+    }
 
 function blockTrump() {
 	var baseState = history.state;
@@ -47,5 +63,41 @@ function scrollFunction() {
 
 
 function hidePost(post) {
+    /* Later this function will replace trump posts with images of cats
+    as well as adjust the post meta to say things like "John Smith posted
+    something vile, we thought you would enjoy this instead" */
     post.style.display = "none";
+}
+
+function getKittens() {
+
+    return kitten;
+}
+
+function flickrErrorCode(error) {
+    var errorCodes = {
+        "1"     : "too many tags in ALL query",
+        "2"     : "Unknown user",
+        "3"     : "Parameterless searches have been disabled",
+        "4"     : "You don't have permission to view this pool",
+        "10"    : "Sorry, the Flickr search API is not currently available",
+        "11"    : "No valid machine tags",
+        "12"    : "Exceeded maximum allowable machine tags",
+        "17"    : "You can only search within your own contacts",
+        "18"    : "Illogical arguments",
+        "100"   : "Invalid API key",
+        "105"   : "Service currently unavailable",
+        "106"   : "Write operation failed",
+        "111"   : "Format 'xxx' not found",
+        "112"   : "Method 'xxx' not found",
+        "114"   : "Invalid SOAP envelope",
+        "115"   : "Invalid XML-RPC method call",
+        "116"   : "Bad URL found"
+    }
+    console.log(errorCodes[error]);
+    window.alert("Hey, there was an issue and the developer would like to know about it. check your browser's console and email me at bretwagner@bwagner-webdev.com");
+}
+
+function flickrRequest() {
+
 }
