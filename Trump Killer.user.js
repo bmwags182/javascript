f836de7524687377d889d1f2179c08072d9b8191
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-24 14:07:06 -0600

    setup greasemonkey http request
    the request is working in that it properly sends to flickr, and flickr
    responds, but since I do not have an API key I will need to get this set
    up and go from there later.

--- Trump Killer.user.js
+++ Trump Killer.user.js
@@ -20,27 +20,30 @@
 // Fix for jQuery so as not to break the page
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+var kittensRequest = {};
 
 var kitten = {
-        "id"        : "",
-        "owner"     : "",
-        "secret"    : "",
-        "server"    : "",
-        "title"     : "",
-        "isPublic"  : "",
-        "isFriend"  : "",
-        "isFamily"  : ""
-    }
+        id        : "",
+        owner     : "",
+        secret    : "",
+        server    : "",
+        title     : "",
+        isPublic  : "",
+        isFriend  : "",
+        isFamily  : ""
+    };
 
 function blockTrump() {
-	var baseState = history.state;
+    var baseState = history.state;
     var posts = document.getElementsByClassName("userContentWrapper _5pcr");
     for(var i=0; i<posts.length; i++) {
-        if (posts[i].innerHTML.toLowerCase().includes("trump") {
+        if (posts[i].innerHTML.toLowerCase().includes("trump")) {
+            // posts[i].style.display = "none";
             hidePost(posts[i]);
             // console.log("hidden");
         } else if (posts[i].innerHTML.toLowerCase().includes("donald") && posts[i].innerHTML.toLowerCase().includes("president")) {
             hidePost(posts[i]);
+            // posts[i].style.display = "none";
         }
     }
     removeAds();
@@ -48,7 +51,7 @@ function blockTrump() {
 
 function removeAds() {
     var ads = document.getElementsByClassName("ego_section");
-    for (var i=0; i<ads.length(); i++) {
+    for (var i=0; i<ads.length; i++) {
         ads[i].style.display = "none";
     }
 }
@@ -67,37 +70,51 @@ function hidePost(post) {
     as well as adjust the post meta to say things like "John Smith posted
     something vile, we thought you would enjoy this instead" */
     post.style.display = "none";
+    // getKittens(); hidden until we can get the page from flickr working
 }
 
 function getKittens() {
+    kittensRequest = {url:"https://www.flickr.com/seach/", onerror:"errors", onload:function(res) { console.log(res.responseText); /* getCats(res.responseText); */ }, headers:{text:"kittens"}};
+    /*
+        The request is not being called at this time because I have hit a snag
+        where I need to get an API key in order to request things from flickr
 
-    return kitten;
+     */
+    GM_xmlhttpRequest(kittensRequest);
 }
+
+function errors(err) {
+    console.log("Something broke " + err);
+}
+
+function getCats(page) {
+    // Right now this isn't being used, I got a response from flickr but I need an API key
+
+}
+
+
 
 function flickrErrorCode(error) {
     var errorCodes = {
-        "1"     : "too many tags in ALL query",
-        "2"     : "Unknown user",
-        "3"     : "Parameterless searches have been disabled",
-        "4"     : "You don't have permission to view this pool",
-        "10"    : "Sorry, the Flickr search API is not currently available",
-        "11"    : "No valid machine tags",
-        "12"    : "Exceeded maximum allowable machine tags",
-        "17"    : "You can only search within your own contacts",
-        "18"    : "Illogical arguments",
-        "100"   : "Invalid API key",
-        "105"   : "Service currently unavailable",
-        "106"   : "Write operation failed",
-        "111"   : "Format 'xxx' not found",
-        "112"   : "Method 'xxx' not found",
-        "114"   : "Invalid SOAP envelope",
-        "115"   : "Invalid XML-RPC method call",
-        "116"   : "Bad URL found"
-    }
+        1     : "too many tags in ALL query",
+        2     : "Unknown user",
+        3     : "Parameterless searches have been disabled",
+        4     : "You don't have permission to view this pool",
+        10    : "Sorry, the Flickr search API is not currently available",
+        11    : "No valid machine tags",
+        12    : "Exceeded maximum allowable machine tags",
+        17    : "You can only search within your own contacts",
+        18    : "Illogical arguments",
+        100   : "Invalid API key",
+        105   : "Service currently unavailable",
+        106   : "Write operation failed",
+        111   : "Format 'xxx' not found",
+        112   : "Method 'xxx' not found",
+        114   : "Invalid SOAP envelope",
+        115   : "Invalid XML-RPC method call",
+        116   : "Bad URL found"
+    };
     console.log(errorCodes[error]);
     window.alert("Hey, there was an issue and the developer would like to know about it. check your browser's console and email me at bretwagner@bwagner-webdev.com");
 }
 
-function flickrRequest() {
-
-}
